d7580d6fccf7c544de85e76e57d5f36859b2fc6c
dudox <trybemark@gmail.com>
2020-07-12 15:17:02 +0100

    @Pages HomePage

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import WHeader from '../../Components/Web/Header/index.component'
+function App() {
+  return (
+    <WHeader></WHeader>
+    
+    
+  );
+}
+
+export default App;
