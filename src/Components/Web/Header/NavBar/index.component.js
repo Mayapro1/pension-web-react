19766087fa1a04b6821d08957578e138b66f5aeb
dudox <trybemark@gmail.com>
2020-07-12 17:34:33 +0100

    fixes

--- src/Components/Web/Header/NavBar/index.component.js
+++ src/Components/Web/Header/NavBar/index.component.js
@@ -1,6 +1,3 @@
-/* eslint-disable react/jsx-no-target-blank */
-/* eslint-disable react/style-prop-object */
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 
 
@@ -12,7 +9,7 @@ class WNavBar extends React.Component {
                 <div className="container"><a className="navbar-brand" href="../../index.html">
                     <img alt="" src={'../../assets/img/brand/dark.svg'} id="navbar-logo"/> </a><button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-main-collapse" aria-controls="navbar-main-collapse" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
                     <div className="collapse navbar-collapse navbar-collapse-overlay" id="navbar-main-collapse">
-                        <div className="position-relative"><button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-main-collapse" aria-controls="navbar-main-collapse" aria-expanded="false" aria-label="Toggle navigation"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-x">
+                        <div className="position-relative"><button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-main-collapse" aria-controls="navbar-main-collapse" aria-expanded="false" aria-label="Toggle navigation"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-x">
                                     <line x1="18" y1="6" x2="6" y2="18"></line>
                                     <line x1="6" y1="6" x2="18" y2="18"></line>
                                 </svg></button></div>
@@ -29,13 +26,13 @@ class WNavBar extends React.Component {
                         </ul>
                         <ul className="navbar-nav align-items-lg-center d-none d-lg-flex ml-lg-auto">
                             <li className="nav-item"><a className="nav-link" href="../../pages/authentication/login-basic.html">Sign in</a></li>
-                            <li className="nav-item"><a href="https://themes.getbootstrap.com/product/quick-website-ui-kit-2/" className="btn btn-sm btn-primary btn-icon ml-3" target="_blank"><span className="btn-inner--icon"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-shopping-bag">
+                            <li className="nav-item"><a href="https://themes.getbootstrap.com/product/quick-website-ui-kit-2/" className="btn btn-sm btn-primary btn-icon ml-3" ><span className="btn-inner--icon"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-shopping-bag">
                                             <path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"></path>
                                             <line x1="3" y1="6" x2="21" y2="6"></line>
                                             <path d="M16 10a4 4 0 0 1-8 0"></path>
                                         </svg></span> <span className="btn-inner--text">Buy now</span></a></li>
                         </ul>
-                        <div className="d-lg-none px-4 text-center"><a href="https://themes.getbootstrap.com/product/quick-website-ui-kit-2/" className="btn btn-block btn-sm btn-primary" target="_blank">Purchase now</a></div>
+                        <div className="d-lg-none px-4 text-center"><a href="https://themes.getbootstrap.com/product/quick-website-ui-kit-2/" className="btn btn-block btn-sm btn-primary" >Purchase now</a></div>
                     </div>
                 </div>
             </nav>
--- src/Components/Web/Header/index.component.js
+++ src/Components/Web/Header/index.component.js
@@ -1,6 +1,3 @@
-/* eslint-disable react/jsx-no-target-blank */
-/* eslint-disable react/style-prop-object */
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import WNavBar from './NavBar/index.component';
 import WBanner from './Banner/index.component';
