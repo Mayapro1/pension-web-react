57e40aa4dfa8eb5a82c52c8b2020c731b8876175
dudox <trybemark@gmail.com>
2020-07-15 16:35:46 +0100

    notification pane sass

--- src/_components/Notifications/FluidPane/index.js
+++ src/_components/Notifications/FluidPane/index.js
@@ -1,11 +1,12 @@
 import React from 'react'; 
 import { NavLink } from 'react-router-dom';
+import './pane.scss';
 
 class FluidPane extends React.Component {
     render() {
       return (
           <div className="text-center py-2" style={{height:"100px !important"}}>
-             <div className="mb-0 pb-0 bg-dark position-absolute w-100 top-0 py-3" >
+             <div className="mb-0 pb-0 bg-dark position-absolute w-100 top-0 py-3 write" >
                  <p className="text-white font-weight-bolder">Find out how PensionAnalytica is #KeepingTheLightsOn for businesses during COVID-19. <NavLink to="/login" className="btn btn-sm rounded-pill btn-outline-warning ">Learn More</NavLink></p>
                  <br/> <br/> <br/>
              </div>
