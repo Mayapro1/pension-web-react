d5370b700e23b5d1fb12e3479ea0a85298ceb4d3
dudox <trybemark@gmail.com>
2020-07-19 22:23:50 +0100

    rename imports

--- src/_pages/Home/index.js
+++ src/_pages/Home/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import WHeader from "../../_components/Web/Header/index.component";
 import WServiceCard from "../../_components/Web/Service/Card/index.component";
-import Login from "../Auth/login.component";
-import Register from "../Auth/register.component";
+import Login from "../_auth/login";
+import Register from "../_auth/register";
 import Pricing from "../Pricing/index.component";
 import CreateProduct from "../Create-product/index.component";
+import Recover from '../_auth/recover'
 
 function App() {
   return (
@@ -22,6 +23,10 @@ function App() {
 
         <Route exact path="/register">
           <Register />
+        </Route>
+
+        <Route exact path="/recover">
+          <Recover />
         </Route>
 
         <Route exact path="/pricing">
