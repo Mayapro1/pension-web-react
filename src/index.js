94abff5be066940d60ebd6503aa4b913b93936c0
dudox <trybemark@gmail.com>
2020-07-20 15:17:37 +0100

    add json web token checker and bearer authorization

--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-
 import Store from "./_reducers/store";
-
 import "./index.css";
 import App from "./_pages/Home/index";
 import * as serviceWorker from "./serviceWorker";
+import { setCurrentUser } from "./_actions/auth.actions";
+import jwt from 'jsonwebtoken';
+import setAuthorizationToken from "./_utils/setAuthorizationToken";
+
 const store = Store();
+
+if(localStorage.jwtToken){
+  setAuthorizationToken(localStorage.getItem('jwtToken'));
+  store.dispatch(setCurrentUser(jwt.decode(localStorage.jwtToken)))
+}
 
 ReactDOM.render(
   <React.StrictMode>
